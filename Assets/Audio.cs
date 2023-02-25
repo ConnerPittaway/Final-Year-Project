895dc46236f26dba0eaf1eec50974e2aff5bf82e
Conner Pittaway <57526209+ConnerPittaway@users.noreply.github.com>
2023-02-25 19:39:25 +0000

    Audio Integration: To:Do change buttons  to function in UI manager

---
+++ Assets/Audio.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Audio : MonoBehaviour
+{
+    public static Audio Instance;
+    public Sounds[] musicSound, sfxSounds;
+    public AudioSource backgroundSource, sfxSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Start()
+    {
+        PlayMusic("Background Music Track 1");
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sounds s = Array.Find(musicSound, x => x.soundName == name);
+
+        if (s == null)
+        {
+            Debug.Log("No Sound Found");
+        }
+        else
+        {
+            backgroundSource.clip = s.audioClip;
+            backgroundSource.Play();
+        }
+    }
+
+    public void PlaySFX(string name)
+    {
+        Sounds s = Array.Find(sfxSounds, x => x.soundName == name);
+
+        if (s == null)
+        {
+            Debug.Log("No Sound Found");
+        }
+        else
+        {
+            sfxSource.PlayOneShot(s.audioClip);
+        }
+    }
+}
